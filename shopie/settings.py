cd1c9d0716d61f57cfe695c5a3d41fa4ed7792d5
pranjalpatidar21 <82169970+pranjalpatidar21@users.noreply.github.com>
2021-08-29 14:54:53 +0530

    Update settings.py

--- shopie/settings.py
+++ shopie/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ug3-#y_5_$7jj6b#ft)0s0yp3ry&0u148-qty4syukcwakggio'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
