17bbc762d7e509e146f4f2f03d4b22f03b70a3dd
pranjalpatidar21 <82169970+pranjalpatidar21@users.noreply.github.com>
2021-08-29 15:00:46 +0530

    Update settings.py

--- shopie/settings.py
+++ shopie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ug3-#y_5_$7jj6b#ft)0s0yp3ry&0u148-qty4syukcwakggio
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','shopie01.herokuapp.com/']
 
 
 # Application definition
